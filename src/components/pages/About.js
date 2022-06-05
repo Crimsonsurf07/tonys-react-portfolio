9bedda8bdde9070af51a0689e61fd54626c17c21
Tony Taylor <73037339+Crimsonsurf07@users.noreply.github.com>
2022-06-05 10:31:06 -0400

    photo added to about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Selfie from '../../assets/images/Selfie.png';
 
 export default function About() {
   return (
@@ -14,6 +15,18 @@ export default function About() {
         graduate from the UNC Chapel Hill Coding Bootcamp and pursue a new
         challenging yet rewarding career path!
       </p>
+      <div>
+        <img
+        src={Selfie}
+        alt="Selfie wearing black polo shirt"
+        title="Selfie"
+        id="Selfie"
+        height={500}
+        width={500}
+        style={{ borderRadius: 2700 }}
+        />
+      </div>
     </div>
+    
   );
 }
